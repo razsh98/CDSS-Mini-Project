df6a190e1fdd38e85e48f81d3481151a4c906c4f
Raz <razsh98@gmail.com>
2020-06-23 11:21:22 +0300

    code reformatting

--- Delete.py
+++ Delete.py
@@ -3,10 +3,13 @@ from tkinter import messagebox
 from UI_Elements import *
 import pandas as pd
 
+dvir_path = "C:\\Users\\dvir levi\\PycharmProjects\\CDSS-Mini-Project\\project_db_test_publish.xlsx"
+raz_path = "C:/_Information Systems Engineering/_Senior Year/Semester H/Decision Support Systems in Medicine/" \
+           "Assignments/Mini Project/project_db_test_publish.xlsx"
 
 
 def run_window():
-    global ent_name,ent_date,ent_hour,ent_minute,lbl_vaule,DB
+    global ent_name,ent_date,ent_hour,ent_minute,lbl_value,DB
 
     delete_window = tk.Tk()
     delete_window.geometry("500x350")
@@ -14,10 +17,10 @@ def run_window():
     ent_name = create_entry(col=3, row=1, text="name", window=delete_window)
 
     lbl_date = create_label(col=2, row=2, text="date", window=delete_window)
-    ent_date=create_DateEntry(col=3,row=2,window=delete_window)
+    ent_date=create_DateEntry(col=3, row=2, window=delete_window)
     lbl_time = create_label(col=2, row=3, text="hh:mm", window=delete_window)
-    ent_hour=create_Spinbox(col=3,row=3,max=23,min=0,window=delete_window)
-    ent_minute=create_Spinbox(col=4,row=3,max=59,min=0,window=delete_window)
+    ent_hour=create_Spinbox(col=3, row=3, max=23,min=0,window=delete_window)
+    ent_minute=create_Spinbox(col=4, row=3, max=59,min=0,window=delete_window)
 
     global ent_date_view, ent_hour_view, ent_minute_view
     lbl_date_view = create_label(col=2, row=4, text="date view", window=delete_window)
@@ -27,12 +30,13 @@ def run_window():
     ent_minute_view = create_Spinbox(col=4, row=5, max=59, min=0, window=delete_window)
 
     btn_submit = create_button(col=4, row=7, text="submit", command=delete, window=delete_window)
-    DB=pd.read_excel("C:\\Users\\dvir levi\\PycharmProjects\\CDSS-Mini-Project\\project_db_test_publish.xlsx")
-    lbl_vaule = create_label(col=4, row=5,text="", window=delete_window)
+    DB = pd.read_excel(raz_path)
+    lbl_value = create_label(col=4, row=5, text="", window=delete_window)
     delete_window.mainloop()
 
+
 def delete():
-    if DB==None:
+    if DB is None:
         show_alert(title="DB loading error", info="cant load th BI DB")
     name=ent_name.get()
     date=ent_date.get()
@@ -42,8 +46,9 @@ def delete():
     if not fine:
         show_alert(title="input error", info="empty name or illegal time")
 
+
 def input_check(name, date, hours,minutes):
-    if name=="" or not isinstance(hours,int) or not isinstance(minutes,int):
-        return 1==0
+    if name == "" or not isinstance(hours, int) or not isinstance(minutes, int):
+        return 1 == 0
 
 # run_window()
