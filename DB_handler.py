dd12d567df762286a1f2dd99a57222755e18ae1d
zvig23 <44335990+zvig23@users.noreply.github.com>
2020-06-24 16:22:13 +0200

    add db handler on retrieve

--- DB_handler.py
+++ DB_handler.py
@@ -18,15 +18,33 @@ def retrive():
     return answer
 
 def retrive_history():
-    DB = pd.read_excel(dvir_path)
-    DB=DB.query("First_name  == 'Eyal'")
-    DB=DB.query("Last_name == 'Rothman'")
-    DB=DB.query("Valid_start_time == '2018-05-17 13:11'")
-    DB=DB.query("Transaction_time <= '2020-05-21 10:00'")
+    pass
+    # DB = pd.read_excel(dvir_path)
+    # DB=DB.query("First_name  == 'Eyal'")
+    # DB=DB.query("Last_name == 'Rothman'")
+    # DB=DB.query("LOINC_NUM == '11218-5'")
+    # DB=DB.query("Valid_start_time >= '2018-05-17 13:11'")
+    # DB=DB.query("Valid_start_time <= '2018-05-17 13:11'")
+    #
+    #
+    # q1="select * from DB ORDER BY Transaction_time DESC ; "
+    # answer=ps.sqldf(q1, locals())
+    # return answer
 
-    q1="select * from DB ORDER BY Transaction_time DESC LIMIT 1; "
+def delete():
+    DB = pd.read_excel(dvir_path)
+    q1="update DB set deleted=1" \
+       " where Valid_start_time=='2018-05-17 13:11' and Transaction_time == '2020-05-21 10:00'"
+    answer=ps.sqldf(q1, locals())
+
+def update():
+    DB = pd.read_excel(dvir_path)
+    q1 ="insert into BD (First_name,Last_name,LOINC_NUM,Value,Unit,Valid_start_time,Transaction_time,deleted)" \
+        ""
     answer=ps.sqldf(q1, locals())
     return answer
 
 
-retrive()
+
+
+print(update())
