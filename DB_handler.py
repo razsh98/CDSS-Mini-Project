5f2e0bbd375b7aab8dc1a975fe7df9e1a73c7174
razsh98 <44240102+razsh98@users.noreply.github.com>
2020-08-03 18:53:44 +0300

    implemented retrieve + retrieval_history

--- DB_handler.py
+++ DB_handler.py
@@ -5,49 +5,42 @@ dvir_path = "C:\\Users\\dvir levi\\PycharmProjects\\CDSS-Mini-Project1\\project_
 raz_path = "project_db_test_publish.xlsx"
 
 
-def retrieve():
+def retrieve(first_name, last_name, valid_start_time, transaction_time, limit=None):
     DB = pd.read_excel(raz_path)
-    DB = DB.query("First_name  == 'Eyal'")
-    DB = DB.query("Last_name == 'Rothman'")
-    DB = DB.query("Valid_start_time == '2018-05-17 13:11'")
-    DB = DB.query("Transaction_time <= '2020-05-21 10:00'")
+    # DB = DB.query("First_name  == 'Eyal'")
+    # DB = DB.query("Last_name == 'Rothman'")
+    # DB = DB.query("Valid_start_time == '2018-05-17 13:11'")
+    # DB = DB.query("Transaction_time <= '2020-05-21 10:00'")
 
-    q1 = "select * from DB ORDER BY Transaction_time DESC LIMIT 1; "
+    DB = DB.query("First_name == '" + first_name + "'")
+    DB = DB.query("Last_name == '" + last_name + "'")
+    DB = DB.query("Valid_start_time >= '" + valid_start_time + "'")
+    DB = DB.query("Transaction_time >= '" + transaction_time + "'")
+
+    if limit is None:
+        limit_clause = " ;"
+    else:
+        limit_clause = "LIMIT " + limit + " ;"
+
+    q1 = "SELECT * FROM DB ORDER BY Transaction_time DESC " + limit_clause
     answer = ps.sqldf(q1, locals())
     return answer
 
 
-def retrieve_history():
-    pass
-    # DB = pd.read_excel(dvir_path)
-    # DB=DB.query("First_name  == 'Eyal'")
-    # DB=DB.query("Last_name == 'Rothman'")
-    # DB=DB.query("LOINC_NUM == '11218-5'")
-    # DB=DB.query("Valid_start_time >= '2018-05-17 13:11'")
-    # DB=DB.query("Valid_start_time <= '2018-05-17 13:11'")
-    #
-    #
-    # q1="select * from DB ORDER BY Transaction_time DESC ; "
-    # answer=ps.sqldf(q1, locals())
-    # return answer
+def retrieve_history(first_name, last_name, valid_start_time, transaction_time):
+    return retrieve(first_name, last_name, valid_start_time, transaction_time)
 
 
 def delete():
     DB = pd.read_excel(dvir_path)
-    q1 = "update DB set deleted=1" \
-         " where Valid_start_time=='2018-05-17 13:11' and Transaction_time == '2020-05-21 10:00'"
+    q1 = "UPDATE DB SET deleted=1" \
+         " WHERE Valid_start_time=='2018-05-17 13:11' AND Transaction_time == '2020-05-21 10:00'"
     answer = ps.sqldf(q1, locals())
 
 
 def update():
     DB = pd.read_excel(dvir_path)
-    q1 = "insert into BD (First_name,Last_name,LOINC_NUM,Value,Unit,Valid_start_time,Transaction_time,deleted)" \
+    q1 = "insert into DB (First_name,Last_name,LOINC_NUM,Value,Unit,Valid_start_time,Transaction_time,deleted)" \
          ""
     answer = ps.sqldf(q1, locals())
     return answer
-
-
-# print(update())
-# print(delete())
-print(retrieve())
-# print(retrieve_history())
--- Retrieval.py
+++ Retrieval.py
@@ -1,60 +1,114 @@
-import pandas as pd
-
+import DB_handler
 from UI_Elements import *
 
 dvir_path = "C:\\Users\\dvir levi\\PycharmProjects\\CDSS-Mini-Project\\project_db_test_publish.xlsx"
 raz_path = "project_db_test_publish.xlsx"
 
+ent_first_name = ent_last_name = ent_date = ent_hour = ent_minute = lbl_value = tk.Entry()
+ent_date_view = ent_hour_view = ent_minute_view = tk.Entry()
+
 
 def run_window():
-    global ent_name, ent_date, ent_hour, ent_minute, lbl_value, DB
+    global ent_first_name, ent_last_name, ent_date, ent_hour, ent_minute, lbl_value
     global ent_date_view, ent_hour_view, ent_minute_view
 
     retrieval_window = tk.Tk()
     retrieval_window.geometry("500x350")
-    lbl_name = create_label(col=2, row=1, text="name patient", window=retrieval_window)
-    ent_name = create_entry(col=3, row=1, text="name", window=retrieval_window)
-    lbl_date = create_label(col=2, row=2, text="date", window=retrieval_window)
-    ent_date = create_DateEntry(col=3, row=2, window=retrieval_window)
-    lbl_time = create_label(col=2, row=3, text="hh:mm", window=retrieval_window)
-    ent_hour = create_spinbox(col=3, row=3, max=23, min=0, window=retrieval_window)
-    ent_minute = create_spinbox(col=4, row=3, max=59, min=0, window=retrieval_window)
+    lbl_first_name = create_label(col=2, row=1, text="First Name", window=retrieval_window)
+    ent_first_name = create_entry(col=3, row=1, text="first_name", window=retrieval_window)
+    lbl_last_name = create_label(col=2, row=2, text="Last Name", window=retrieval_window)
+    ent_last_name = create_entry(col=3, row=2, text="last_name", window=retrieval_window)
+    lbl_date = create_label(col=2, row=3, text="date", window=retrieval_window)
+    ent_date = create_DateEntry(col=3, row=3, window=retrieval_window)
+    lbl_time = create_label(col=2, row=4, text="hh:mm", window=retrieval_window)
+    ent_hour = create_spinbox(col=3, row=4, max=23, min=0, window=retrieval_window)
+    ent_minute = create_spinbox(col=4, row=4, max=59, min=0, window=retrieval_window)
 
-    lbl_date_view = create_label(col=2, row=4, text="date view", window=retrieval_window)
-    ent_date_view = create_DateEntry(col=3, row=4, window=retrieval_window)
-    lbl_time_view = create_label(col=2, row=5, text="hh:mm", window=retrieval_window)
-    ent_hour_view = create_spinbox(col=3, row=5, max=23, min=0, window=retrieval_window)
-    ent_minute_view = create_spinbox(col=4, row=5, max=59, min=0, window=retrieval_window)
+    lbl_date_view = create_label(col=2, row=5, text="date view", window=retrieval_window)
+    ent_date_view = create_DateEntry(col=3, row=5, window=retrieval_window)
+    lbl_time_view = create_label(col=2, row=6, text="hh:mm", window=retrieval_window)
+    ent_hour_view = create_spinbox(col=3, row=6, max=23, min=0, window=retrieval_window)
+    ent_minute_view = create_spinbox(col=4, row=6, max=59, min=0, window=retrieval_window)
 
-    btn_submit = create_button(col=4, row=6, text="submit", command=retrieve, window=retrieval_window)
-    DB = pd.read_excel(raz_path)
-    lbl_value = create_label(col=4, row=8, text="", window=retrieval_window)
+    btn_submit = create_button(col=4, row=7, text="submit", command=retrieve, window=retrieval_window)
+    lbl_value = create_label(col=4, row=9, text="", window=retrieval_window)
     retrieval_window.mainloop()
 
 
 def retrieve():
-    name = ent_name.get()
-    date = ent_date.get()
-    hours = ent_hour.get()
-    minutes = ent_minute.get()
-    date_view = ent_date_view.get()
-    hours_view = ent_hour_view.get()
-    minutes_view = ent_minute_view.get()
-    fine0 = input_check(name, date, hours, minutes)
-    fine1 = input_check(name, date_view, hours_view, minutes_view)
-    # if not fine0 or not fine1:
-    #     show_alert(title="input error", info="empty name or illegal time")
-    #     return
+    first_name = ent_first_name.get()
+    last_name = ent_last_name.get()
 
-    answer = ""
-    lbl_value['text'] = answer
-    print("hello")
+    valid_start_time = parse_date_time_input(ent_date, ent_hour, ent_minute)
+
+    transaction_time = parse_date_time_input(ent_date_view, ent_hour_view, ent_minute_view)
+
+    if validate_not_empty(first_name) and \
+            validate_not_empty(last_name) and \
+            valid_start_time != "" and \
+            transaction_time != "":
+
+        answer = ""
+        lbl_value['text'] = answer
+        print("hello")
+        DB_handler.retrieve(
+            first_name=first_name,
+            last_name=last_name,
+            valid_start_time=valid_start_time,
+            transaction_time=transaction_time,
+            limit=1
+        )
 
 
-def input_check(name, date, hours, minutes):
-    if name == "" or not isinstance(hours, int) or not isinstance(minutes, int):
-        return 1 == 0
+def parse_date_time_input(date, hours, minutes):
+
+    date_val = date.get()
+    hours_val = hours.get()
+    minutes_val = minutes.get()
+
+    if validate_dates(hours_val, minutes_val):
+        if int(hours_val) < 10:
+            hours_val = "0" + hours_val
+        if int(minutes_val) < 10:
+            minutes_val = "0" + minutes_val
+        return date_val + " " + hours_val + ":" + minutes_val
+    else:
+        return ""
 
 
-# print(pd.read_excel("C:\\Users\\dvir levi\\PycharmProjects\\CDSS-Mini-Project\\project_db_test_publish.xlsx"))
+def validate_dates(hours, minutes):
+    if hours is not None:
+        if hours.isdigit():
+            hours = int(hours)
+            if hours >= 0:
+                if hours < 24:
+                    if minutes is not None:
+                        if minutes.isdigit():
+                            minutes = int(minutes)
+                            if minutes >= 0:
+                                if minutes < 60:
+                                    return True
+    show_alert(
+        title="Input Error",
+        info="Error: Illegal Time!"
+    )
+    return False
+
+
+def validate_not_empty(field):
+    if field is not None:
+        if field != "":
+            return True
+    show_alert(
+        title="Input Error",
+        info="Error: Mandatory fields are empty!"
+    )
+    return False
+
+
+# def input_check(name, date, hours, minutes):
+#     if name == "" or not isinstance(hours, int) or not isinstance(minutes, int):
+#         return 1 == 0
+
+
 run_window()
